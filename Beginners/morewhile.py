769b0eadfa46700fb73cf94d335ed54bb96d8e63
Aminah92 <39123465+Aminah92@users.noreply.github.com>
2020-07-29 13:50:30 +0100

    Add files via upload

---
+++ Beginners/morewhile.py
@@ -0,0 +1,12 @@
+#creating an infinite loop
+#using the while loop
+
+mood = input('Are you hungry?: ')
+while mood == ‘Yes’:
+    print('I am still Hungry')
+
+#So the while loop can be stopped using ctrl + c on the keyboard.
+#This is called a keyboard interrupt.
+#It is used for stopping an infinite loop such as the while loop.
+
+
