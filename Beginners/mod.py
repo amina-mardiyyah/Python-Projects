8e2f74a7044ff92dbdbb65c60c97f60333a66628
Aminah92 <39123465+Aminah92@users.noreply.github.com>
2020-09-10 18:13:42 +0100

    Add files via upload

---
+++ Beginners/mod.py
@@ -0,0 +1,16 @@
+#Calculator module
+#Function for Addition
+def add(num1,num2):
+	return num1 + num2
+    
+#Function for subtraction
+def sub(num1,num2):
+	return num1 - num2
+    
+#Function for Mutiplication
+def mult(num1,num2):
+	return num1 * num2
+
+#Function for Division
+def div(num1,num2):
+	return num1 / num2
