66980eff0286ef4a448b5c2ebc43e63d192b65d4
Aminah92 <39123465+Aminah92@users.noreply.github.com>
2020-09-10 18:16:30 +0100

    Create calc_modified.py

---
+++ Beginners/calc_modified.py
@@ -0,0 +1,39 @@
+#A simple Calculator
+
+#import the mod module using any alias of choice
+from mod import *
+
+# Define a function for running the calculator
+
+def calc_run():
+        # Define variables for two numbers using the int and input function
+        num1 = int(input('Please enter a number: '))
+        num2 = int(input('Please enter another number: '))
+        #Define a variable for choosing operator
+        #Use multi-line text format
+        operator = input("""What type of mathematical operation would you like to carry out?
+                        Select an option by entering the corresponding number for the desired operation.
+                     1. Addition
+                     2. Subtraction
+                     3. Multiplication
+                     4. Division?:
+                     Ans:  """)
+        #Use conditional statements for decision making
+        #Call out each operator from the module using the dot method
+                         
+        if operator == '1':
+                return add(num1,num2)
+        elif operator == '2':
+                return sub(num1,num2)
+        elif operator == '3':
+                return mult(num1,num2)
+        elif operator == '4':
+                return div(num1,num2)
+        else:
+                return "Wrong input! Kindly enter the correct option"
+
+
+
+
+
+  
