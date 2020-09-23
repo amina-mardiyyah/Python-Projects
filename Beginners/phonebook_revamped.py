3be31a393b955520beecc336da95bfc860ded8df
Amina Mardiyyah Rufai <39123465+Aminah92@users.noreply.github.com>
2020-09-23 19:00:16 +0100

    Create phonebook_revamped.py

---
+++ Beginners/phonebook_revamped.py
@@ -0,0 +1,15 @@
+#Import the pycontact module
+
+from pycontact_module import *
+
+
+#Define a function to initiate the phonebook program
+#Call the menu function from pycontact
+def Phonebook():
+            
+        #call menu function.
+    contacts={}
+    menu()
+
+#Call the Phonebook function to run the program
+Phonebook()
